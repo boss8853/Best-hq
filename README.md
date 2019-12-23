8ae5dc4382c8d8357d09bb0f937bc7a28db46f89
boss8853 <58292547+boss8853@users.noreply.github.com>
2019-12-23 22:51:29 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Best-hq
+# loco
---
+++ newbuy.py
@@ -0,0 +1,326 @@
+  
+'''
+using discord.py version 1.0.0a
+'''
+import discord
+import asyncio
+import re
+import multiprocessing
+import threading
+import concurrent
+
+BOT_OWNER_ROLE = 'The Best | | Runner' # change to what you need
+#BOT_OWNER_ROLE_ID ="597332392637890571", 
+"569420128794443776", 
+"607613349491900436" 
+  
+ 
+
+ 
+oot_channel_id_list = ["620842231229841421",  #gnew
+"459842150323060736",   #dim
+"568617830258442255",   #revo
+"655723300131176468",   #boss
+"513818250652680213"    #wold
+
+]
+
+
+answer_pattern = re.compile(r'(not|n)?([1-3]{1})(\?)?(cnf)?(\?)?$', re.IGNORECASE)
+
+apgscore = 200
+nomarkscore = 100
+markscore = 50
+
+async def update_scores(content, answer_scores):
+    global answer_pattern
+
+    m = answer_pattern.match(content)
+    if m is None:
+        return False
+
+    ind = int(m[2])-1
+
+    if m[1] is None:
+        if m[3] is None:
+            if m[4] is None:
+                answer_scores[ind] += nomarkscore
+            else: # apg
+                if m[5] is None:
+                    answer_scores[ind] += apgscore
+                else:
+                    answer_scores[ind] += markscore
+
+        else: # 1? ...
+            answer_scores[ind] += markscore
+
+    else: # contains not or n
+        if m[3] is None:
+            answer_scores[ind] -= nomarkscore
+        else:
+            answer_scores[ind] -= markscore
+
+    return True
+
+class SelfBot(discord.Client):
+
+    def __init__(self, update_event, answer_scores):
+        super().__init__()
+        global oot_channel_id_list
+        #global wrong
+        self.oot_channel_id_list = oot_channel_id_list
+        self.update_event = update_event
+        self.answer_scores = answer_scores
+
+    async def on_ready(self):
+        print("======================")
+        print("Moonlite Trivia Self Bot")
+        print("Connected to discord.")
+        print("User: " + self.user.name)
+        print("ID: " + str(self.user.id))
+
+    # @bot.event
+    # async def on_message(message):
+    #    if message.content.startswith('-debug'):
+    #         await message.channel.send('d')
+
+        def is_scores_updated(message):
+            if message.guild == None or \
+                str(message.channel.id) not in self.oot_channel_id_list:
+                return False
+
+            content = message.content.replace(' ', '').replace("'", "")
+            m = answer_pattern.match(content)
+            if m is None:
+                return False
+
+            ind = int(m[2])-1
+
+            if m[1] is None:
+                if m[3] is None:
+                    if m[4] is None:
+                        self.answer_scores[ind] += nomarkscore
+                    else: # apg
+                        if m[5] is None:
+                            self.answer_scores[ind] += apgscore
+                        else:
+                            self.answer_scores[ind] += markscore
+
+                else: # 1? ...
+                    self.answer_scores[ind] += markscore
+
+            else: # contains not or n
+                if m[3] is None:
+                    self.answer_scores[ind] -= nomarkscore
+                else:
+                    self.answer_scores[ind] -= markscore
+
+            return True
+
+        while True:
+            await self.wait_for('message', check=is_scores_updated)
+            self.update_event.set()
+
+class Bot(discord.Client):
+
+    def __init__(self, answer_scores):
+        super().__init__()
+        self.bot_channel_id_list = []
+        self.embed_msg = None
+        self.embed_channel_id = None
+        #global wrong
+        self.answer_scores = answer_scores
+
+        # embed creation
+        self.embed=discord.Embed(title= "__**HQ TRIVIA**__", description="**hq Deep searching.....**", color=0xFF00FF)
+        self.embed.set_author(name ='',url=' ',icon_url='')
+        self.embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/650965552965353482/656544268357206036/images_1.png")
+        
+        
+        self.embed.add_field(name="**__ANSWER 1__**", value="~~0.0~~", inline=False)
+        
+        self.embed.add_field(name="**__ANSWER 2__**", value="~~0.0~~", inline=False)
+        
+        self.embed.add_field(name="**__ANSWER 3__**", value="~~0.0~~", inline=False)
+        
+        
+        self.embed.set_footer(text=f"Made by ♥ ⎝⧹𝗗𝗿. BOSS™╱⎠ & M̸R̸.̸G̸E̸N̸I̸U̸S̸ K̸I̸N̸G̸"                , \
+            icon_url="https://cdn.discordapp.com/attachments/626463527301021735/638041312100220948/JPEG_20191002_104501.jpg")
+
+        #await self.bot.add_reaction(embed,':spy:')
+
+
+    async def clear_results(self):
+        for i in range(len(self.answer_scores)):
+            self.answer_scores[i]=0
+
+    async def update_embeds(self):
+      #  global wrong
+
+         
+
+        one_check = ""
+        two_check = ""
+        three_check = ""
+        mark_check_one=""
+        mark_check_two=""
+        mark_check_three=""
+        best_answer = ' :hourglass: '
+        
+        
+        lst_scores = list(self.answer_scores)
+        
+
+        highest = max(lst_scores)
+        best_answer = ' :hourglass: '
+        lowest = min(lst_scores)
+        answer = lst_scores.index(highest)+1
+        #global wrong             
+
+        if highest > 0:
+            if answer == 1:
+                one_check = ":white_check_mark:"
+                mark_check_one = ":one:"
+                best_answer = ':one:'
+            else:
+                one_check = ""
+
+            if answer == 2:
+                two_check = ":white_check_mark:"
+                mark_check_one = ":two:"
+                best_answer = ':two:'
+            else:
+                two_check = ""
+
+            if answer == 3:
+                three_check = ":white_check_mark:"
+                mark_check_one = ":three:"
+                best_answer = ':three:'
+            else:
+                three_check = ""
+
+            
+
+        #if lowest < 0:
+            #if answer == 1:
+                #one_cross = ":x:"
+            #if answer == 2:
+                #two_cross = ":x:"
+            #if answer == 3:
+                #three_cross = ":x:"            
+ 
+        self.embed.set_field_at(0, name="**__ANSWER  1__**", value="**{0}.0**{1}".format(lst_scores[0], one_check))
+        
+        self.embed.set_field_at(1, name="**__ANSWER  2__**", value="**{0}.0**{1}".format(lst_scores[1], two_check))
+        
+        self.embed.set_field_at(2, name="**__ANSWER  3__**", value="**{0}.0**{1}".format(lst_scores[2], three_check),)
+
+
+        if self.embed_msg is not None:
+            await self.embed_msg.edit(embed=self.embed)
+
+    async def on_ready(self):
+        print("==============")
+        print("Moonlite Trivia")
+        print("Connected to discord.")
+        print("User: " + self.user.name)
+        print("ID: " + str(self.user.id))
+
+        await self.clear_results()
+        await self.update_embeds()
+        #await self.change_presence(activity=discord.Game(name='with '+str(len(set(self.get_all_members())))+' users'))
+        await self.change_presence(activity=discord.Activity(type=1,name='with HQ ANSWER!!'))
+
+    async def on_message(self, message):
+
+        # if message is private
+        if message.author == self.user or message.guild == None:
+            return
+
+        if message.content.lower() == "hq":
+            await message.delete()
+            if BOT_OWNER_ROLE in [role.name for role in message.author.roles]:
+                self.embed_msg = None
+                await self.clear_results()
+                await self.update_embeds()
+                self.embed_msg = \
+                    await message.channel.send('',embed=self.embed)
+                await self.embed_msg.add_reaction("✅")
+                # await self.embed_msg.add_reaction(":white_check_mark:")
+                await self.embed_msg.add_reaction("❌")
+
+                self.embed_channel_id = message.channel.id
+            else:
+                await message.channel.send("**Lol** You Not Have permission To Use This **cmd!** :stuck_out_tongue_winking_eye:")
+            return
+
+        if message.content.startswith('*elp'):
+          await message.delete()
+          if BOT_OWNER_ROLE in [role.name for role in message.author.roles]:
+           embed = discord.Embed(title="**__HELP COMMANDS__**", description="**__HOW RUN BOT__**", color=0x00ff00)
+           embed.add_field(name="**__SUPPORT GAME__** ", value="**Loco\nSwag-iq\nConfett-India\nHQ Trivia**", inline=False)
+           embed.add_field(name="**__WHEN QUESTION COME PUT COMMAND__**", value=" * **__is COMMAND WORK FOR ALL SUPPORT GAME__**", inline=False)
+           await message.channel.send(embed=embed)
+          
+
+        # process votes
+        if message.channel.id == self.embed_channel_id:
+            content = message.content.replace(' ', '').replace("'", "")
+            updated = await update_scores(content, self.answer_scores)
+            if updated:
+                await self.update_embeds()
+
+def bot_with_cyclic_update_process(update_event, answer_scores):
+
+    def cyclic_update(bot, update_event):
+        f = asyncio.run_coroutine_threadsafe(bot.update_embeds(), bot.loop)
+        while True:
+            update_event.wait()
+            update_event.clear()
+            f.cancel()
+            f = asyncio.run_coroutine_threadsafe(bot.update_embeds(), bot.loop)
+            #res = f.result()
+
+    bot = Bot(answer_scores)
+
+    upd_thread = threading.Thread(target=cyclic_update, args=(bot, update_event))
+    upd_thread.start()
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.start('NjU2NTQ3MTAwNjMyNDE2MjU5.XfkPRw.FwQaNMJrTnRcmtW0753uC3TIZFI'))
+    loop.run_forever()
+
+
+def selfbot_process(update_event, answer_scores):
+
+    selfbot = SelfBot(update_event, answer_scores)
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(selfbot.start('NTYwMDk0Mzk2ODg4OTA3Nzg2.XeZ9OA.KcUv3HmuO8UMZQg5i-A8FE_sjA4',
+                                   bot=False))
+    loop.run_forever()
+
+if __name__ == '__main__':
+
+    # running bot and selfbot in separate OS processes
+
+    # shared event for embed update
+    update_event = multiprocessing.Event()
+
+    # shared array with answer results
+    answer_scores = multiprocessing.Array(typecode_or_type='i', size_or_initializer=3)
+
+    p_bot = multiprocessing.Process(target=bot_with_cyclic_update_process, args=(update_event, answer_scores))
+    p_selfbot = multiprocessing.Process(target=selfbot_process, args=(update_event, answer_scores))
+
+    p_bot.start()
+    p_selfbot.start()
+
+    p_bot.join()
+    p_selfbot.join()
+
+
+
+
+ 
+ 
